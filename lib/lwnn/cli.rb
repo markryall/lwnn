2dc44c2aa9dc5717768d0a4bc8a101c90be81c75
Mark Ryall <mark@ryall.name>
2012-04-17 09:32:11 +1000

    cli now accepts file name arguments (otherwise defaulting to an interactive prompt from stdin)

--- lib/lwnn/cli.rb
+++ lib/lwnn/cli.rb
@@ -1,7 +1,15 @@
 module Lwnn
   class Cli
     def self.run *args
-      while line = gets
+      if args.empty?
+        process $stdin
+      else
+        args.each {|arg| File.open(arg) {|file| process file } }
+      end
+    end
+
+    def self.process io
+      while line = io.gets
         line.chomp!
         return if line == 'exit'
         puts 'Stack: 2'
--- spec/lwnn/cli_spec.rb
+++ spec/lwnn/cli_spec.rb
@@ -4,7 +4,19 @@ require 'lwnn/cli'
 
 describe Lwnn::Cli do
   it 'should abort on reading "exit"' do
-    Lwnn::Cli.should_receive(:gets).and_return 'exit'
+    $stdin.should_receive(:gets).and_return 'exit'
     Lwnn::Cli.run
+  end
+
+  it 'should abort on eof' do
+    $stdin.should_receive(:gets).and_return nil
+    Lwnn::Cli.run
+  end
+
+  it 'should read from file instead of stdin when passed parameters' do
+    io = stub 'io'
+    io.should_receive(:gets).and_return nil
+    File.should_receive(:open).with('foo.lwnn').and_yield io
+    Lwnn::Cli.run 'foo.lwnn'
   end
 end
