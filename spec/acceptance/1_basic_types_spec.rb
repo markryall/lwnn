d91bfe86d236bf4bfaf94b97fc638b4913ea72ab
Mark Ryall <mark@ryall.name>
2013-02-07 08:54:45 +1000

    started moving back to rspec away from cucumber and aruba for acceptance tests

---
+++ spec/acceptance/1_basic_types_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe 'basic types' do
+  include LwnnHelper
+
+  {
+    'a'     => 'a',
+    '2'     => '2',
+    '1 2'   => '1 2',
+    'true'  => 'true',
+    '1 2 +' => '1 2 +'
+  }.each do |input, output|
+    it { input.should evaluate_to output }
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,22 @@
+class LwnnMatcher
+  def initialize expected
+    @expected = expected
+  end
+
+  def matches? input
+    lines = `echo '#{input}' | ./bin/lwnn`.split("\n")
+    match = /State: /.match lines[1]
+    @actual = match.post_match
+    @actual == @expected
+  end
+
+  def failure_message
+    "expected:\n\t#{@expected.inspect}\nactual:\n\t#{@actual.inspect}"
+  end
+end
+
+module LwnnHelper
+  def evaluate_to string
+    LwnnMatcher.new string
+  end
+end
